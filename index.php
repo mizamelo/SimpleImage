27c0db2926a9369aa3ac725bb16914027723ebdd
ebelmiro <eudes_bbj@hotmail.com>
2013-08-18 16:30:50 -0300

    Edições na index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'SimpleImage.php';
+require_once 'SimpleImage/SimpleImage.php';
 
 $simple = new SimpleImage('../img/Test.png');
 $simple->rotate90();
@@ -9,6 +9,7 @@ $simple->merge('../img/merge.jpg', 10, 25);
 $simple->crop(300, 300, 20, 40);
 $simple->setFont('../font/arial.ttf');
 $simple->setFontSize(15);
-$simple->setFontColor('#FF0000');
+$simple->setFontColor('#FF0000'); // ou #F00
 $simple->write('Test', 45, 45);
 $simple->save();
+
